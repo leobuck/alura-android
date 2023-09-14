9981dc8213d988a62865c127a8c2bdbb4b9be52f
leobuck <leobuck12@gmail.com>
2023-09-13 22:16:21 -0300

    Inserindo ícone e App Bar

--- Agenda/app/src/main/java/com/buck/agenda/MainActivity.java
+++ Agenda/app/src/main/java/com/buck/agenda/MainActivity.java
@@ -9,17 +9,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de alunos");
+
         List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "Jose", "Ana"));
 
         ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
