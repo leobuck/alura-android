db3ee25737aaf3916f1d5a9a397352ce40a2b2c5
leobuck <leobuck12@gmail.com>
2023-09-16 11:08:47 -0300

    Salvando alunos

--- Agenda/app/src/main/java/com/buck/agenda/ui/activity/ListaAlunosActivity.java
+++ Agenda/app/src/main/java/com/buck/agenda/ui/activity/ListaAlunosActivity.java
@@ -8,6 +8,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.buck.agenda.R;
+import com.buck.agenda.dao.AlunoDAO;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,10 +22,12 @@ public class ListaAlunosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de alunos");
 
-        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "Jose", "Ana"));
+        AlunoDAO dao = new AlunoDAO();
 
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(
-                this, android.R.layout.simple_list_item_1, alunos));
+                this,
+                android.R.layout.simple_list_item_1,
+                dao.todos()));
     }
 }
