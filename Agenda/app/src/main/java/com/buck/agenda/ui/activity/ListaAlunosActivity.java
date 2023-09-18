e68e83711f7b3001555aa03eaebb0896b856a560
leobuck <leobuck12@gmail.com>
2023-09-18 06:28:35 -0300

    Finalizando fluxo do App

--- Agenda/app/src/main/java/com/buck/agenda/ui/activity/ListaAlunosActivity.java
+++ Agenda/app/src/main/java/com/buck/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,6 +1,8 @@
 package com.buck.agenda.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -9,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.buck.agenda.R;
 import com.buck.agenda.dao.AlunoDAO;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,6 +24,19 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de alunos");
+
+        FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_lista_alunos_fab_novo_aluno);
+        botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ListaAlunosActivity.this, FormularioAlunoActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
         AlunoDAO dao = new AlunoDAO();
 
