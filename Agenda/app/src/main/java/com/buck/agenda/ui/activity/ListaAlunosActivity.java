7e67befc4b035977740cfe7f0a5fdeb29deced17
leobuck <leobuck12@gmail.com>
2023-09-30 09:34:33 -0300

    Notificando mudança do dataset

--- Agenda/app/src/main/java/com/buck/agenda/ui/activity/ListaAlunosActivity.java
+++ Agenda/app/src/main/java/com/buck/agenda/ui/activity/ListaAlunosActivity.java
@@ -4,15 +4,10 @@ import static com.buck.agenda.ui.activity.ConstantesActivities.CHAVE_ALUNO;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.ContextMenu;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -24,9 +19,6 @@ import com.buck.agenda.dao.AlunoDAO;
 import com.buck.agenda.model.Aluno;
 import com.buck.agenda.ui.adapter.ListaAlunosAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -83,9 +75,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
     }
 
     private void atualizaAlunos() {
-        adapter.clear();
-        adapter.addAll(dao.todos());
-        adapter.notifyDataSetChanged();
+        adapter.atualiza(dao.todos());
     }
 
     private void configuraLista() {
@@ -98,7 +88,6 @@ public class ListaAlunosActivity extends AppCompatActivity {
     private void remove(Aluno aluno) {
         dao.remove(aluno);
         adapter.remove(aluno);
-        adapter.notifyDataSetChanged();
     }
 
     private void configuraListenerDeCliquePorItem(ListView listaDeAlunos) {
--- Agenda/app/src/main/java/com/buck/agenda/ui/adapter/ListaAlunosAdapter.java
+++ Agenda/app/src/main/java/com/buck/agenda/ui/adapter/ListaAlunosAdapter.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 public class ListaAlunosAdapter extends BaseAdapter {
     private final List<Aluno> alunos = new ArrayList<>();
-    private Context context;
+    private final Context context;
 
     public ListaAlunosAdapter(Context context) {
         this.context = context;
@@ -38,26 +38,33 @@ public class ListaAlunosAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View viewCriada = LayoutInflater
-                .from(context)
-                .inflate(R.layout.item_aluno, parent, false);
+        View viewCriada = criaView(parent);
         Aluno alunoDevolvido = alunos.get(position);
-        TextView nome = viewCriada.findViewById(R.id.item_aluno_nome);
-        nome.setText(alunoDevolvido.getNome());
-        TextView telefone = viewCriada.findViewById(R.id.item_aluno_telefone);
-        telefone.setText(alunoDevolvido.getTelefone());
+        vincula(viewCriada, alunoDevolvido);
         return viewCriada;
     }
 
-    public void clear() {
-        alunos.clear();
+    private void vincula(View view, Aluno aluno) {
+        TextView nome = view.findViewById(R.id.item_aluno_nome);
+        nome.setText(aluno.getNome());
+        TextView telefone = view.findViewById(R.id.item_aluno_telefone);
+        telefone.setText(aluno.getTelefone());
     }
 
-    public void addAll(List<Aluno> alunos) {
+    private View criaView(ViewGroup parent) {
+        return LayoutInflater
+                .from(context)
+                .inflate(R.layout.item_aluno, parent, false);
+    }
+
+    public void atualiza(List<Aluno> alunos) {
+        this.alunos.clear();
         this.alunos.addAll(alunos);
+        notifyDataSetChanged();
     }
 
     public void remove(Aluno aluno) {
         alunos.remove(aluno);
+        notifyDataSetChanged();
     }
 }
